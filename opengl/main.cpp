651cfe7dc8e9d3e7dfc5d19be8c813c931d45655
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-06-06 00:51:07 +0300

    Initial Commit

---
+++ opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by Vladislav Khudiakov on 6/6/19.
+//  Copyright © 2019 Vladislav Khudiakov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
