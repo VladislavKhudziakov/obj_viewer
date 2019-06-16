93487e9465c6f9de98835291ccaf743b30b6c806
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-06-16 23:59:36 +0300

    fixed texture

--- opengl/engine/Texture.cpp
+++ opengl/engine/Texture.cpp
@@ -26,14 +26,7 @@ namespace Engine
     glTexImage2D(
      GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
     
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
     glGenerateMipmap(GL_TEXTURE_2D);
-    
-    
     
     SOIL_free_image_data(img);
   };
